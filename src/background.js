1fa42b8b12dcdbe1ab5cc156b8f279d2d4d50c89
Akmal <magchuz@pm.me>
2023-01-25 15:27:10 +0700

    Optimize background script

--- src/background.js
+++ src/background.js
@@ -5,36 +5,32 @@
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
-const convertBlobToBase64 = blob => new Promise(resolve => {
-  const reader = new FileReader();
-  reader.readAsDataURL(blob);
-  reader.onloadend = () => {
-      const base64data = reader.result;
-      resolve(base64data);
-  };
+// Setup settings
+chrome.runtime.onInstalled.addListener(async () => {
+  await chrome.storage.local.set({ auto_open: true, auto_solve: true, solve_delay_time: 3000 });
 });
 
+const convertBlobToBase64 = async blob => {
+  const reader = new FileReader();
+  return new Promise((resolve) => {
+      reader.onloadend = () => resolve(reader.result);
+      reader.readAsDataURL(blob);
+  });
+}
 
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
-    var model_url =
-      `https://github.com/Wikidepia/hektCaptcha-extension/releases/download/modelhub/${request}.ort`
-    const fetchModel = async () => {
-      const response = await fetch(model_url);
-      const respBlob = await response.blob();
-      const base64 = await convertBlobToBase64(respBlob);
-
-      sendResponse({ status: response.status, model: base64 });
-    }
-    fetchModel()
+   (async () => {
+      const modelURL = `https://github.com/Wikidepia/hektCaptcha-extension/releases/download/modelhub/${request}.ort`;
+      try {
+        const response = await fetch(modelURL);
+        const blob = await response.blob();
+        const base64 = await convertBlobToBase64(blob);
+        sendResponse({ status: response.status, model: base64 });
+      } catch (error) {
+        sendResponse({ status: "error", message: error.message });
+      }
+    })()
     return true;
   }
 );
-
-(function () {
-    // Setup settings
-    chrome.runtime.onInstalled.addListener(async () => {
-      await chrome.storage.local.set({ auto_open: true, auto_solve: true, solve_delay_time: 3000 });
-    });
-  }
-)()
