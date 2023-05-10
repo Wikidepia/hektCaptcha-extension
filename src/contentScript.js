925539831d79c0c10edd13022f04574d297802c7
Akmal <magchuz@pm.me>
2023-05-11 05:45:58 +0700

    Fix 'select all..' label

--- src/contentScript.js
+++ src/contentScript.js
@@ -359,6 +359,7 @@ function simulateMouseClick(element, clientX = null, clientY = null) {
         .replace('Please click each image containing', '')
         .replace('Please click on all images containing', '')
         .replace('Please click on all images of', '')
+        .replace('Select all images containing', '')
         .trim()
         .replace(/^(a|an)\s+/i, '')
         .replace(/\s+/g, '_')
